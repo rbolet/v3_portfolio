f82ea7a96149d480bc3a903011f293b834c5df95
rbolet <rapha.bolet@gmail.com>
2020-12-23 13:59:34 -0800

    added html skeleton to react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div id="header"></div>
+      <div class="page-container">
+        <div class="before-fold">
+          <div class="row">
+            <div class="title-container">
+              <div class="title">
+                <p>
+                  Rapha
+                  <br />
+                  Bolet
+                </p>
+                <p>
+                  Developer<span id="title-cursor">_</span>
+                </p>
+              </div>
+            </div>
+            <div class="chatbot-container">
+              <div class="chatbot-border">
+                <div class="chatbot-messages-container">
+                  <div class="sent message">
+                    <p>
+                      Message Sent is a super duper long message that fills the container so we
+                      probably should add some margin
+                    </p>
+                  </div>
+                  <div class="received message">
+                    <p>
+                      Message Received is a super duper long message that fills the container so we
+                      probably should add some margin
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="chatbot-border chatbot-input-area">
+                <div class="thinking">
+                  <span>.</span>
+                  <span>.</span>
+                  <span>.</span>
+                </div>
+                <form>
+                  <input class="chatbot-text-input" />
+                  <button>
+                    <i class="far fa-paper-plane"></i>
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
